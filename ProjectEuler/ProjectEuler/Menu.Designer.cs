3e98c5b197cd1af11080d10d67dea7fc28fc8d3a
Jordan McCall <jordanmccall@hotmail.com>
2015-09-20 21:54:24 +0100

    Renamed Form1.cs to Menu.cs
    Menu.cs will be used to connect all other problem solutions.

--- ProjectEuler/ProjectEuler/Menu.Designer.cs
+++ ProjectEuler/ProjectEuler/Menu.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProjectEuler
 {
-    partial class Form1
+    partial class Menu
     {
         /// <summary>
         /// Required designer variable.
--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -16,7 +16,7 @@ namespace ProjectEuler
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Menu());
         }
     }
 }

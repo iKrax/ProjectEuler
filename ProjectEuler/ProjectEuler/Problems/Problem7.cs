f4a5e1cb2fb4aa6c32e26bfa3fb26d7ab65b1525
Jordan McCall <jordanmccall@hotmail.com>
2015-09-27 23:49:05 +0100

    Added Problem 7 Solution

---
+++ ProjectEuler/ProjectEuler/Problems/Problem7.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    class Problem7 : Problem
+    {        
+        public override string name { get { return "Problem 7"; } }
+        public override string description { get { return "10001st prime"; } }
+
+        public override object execute()
+
+        {
+            int nPrimes = 1;
+            int n = 1;
+
+            while (nPrimes < 10001)
+            {
+                n = n + 2;
+                if (isPrime(n))
+                {
+                    nPrimes++;
+                }
+            }
+            return n;
+        }
+
+        public bool isPrime(int n)
+        {
+            if ((n % 2 == 0) && (n != 2))
+            {
+                return false;
+            }
+            int c = 3;
+            while ((c * c) <= n)
+            {
+                if (n % c == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    c += 2;
+                }
+            }
+            return true;
+        }
+    }
+}

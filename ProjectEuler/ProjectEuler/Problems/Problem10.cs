c087d509fa804fb27de0156b01bb0361d803fb2a
Jordan McCall <jordanmccall@hotmail.com>
2015-09-28 17:27:39 +0100

    Added Problem 10
    Yay 10 in a row :dancers:

---
+++ ProjectEuler/ProjectEuler/Problems/Problem10.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    class Problem10 : Problem
+    {
+        public override string name { get { return "Problem 10"; } }
+        public override string description { get { return "Summation of primes"; } }
+
+        public override object execute()
+        {
+            long sum = 2;
+
+            for (int i = 3; i < 2000000; i += 2)
+            {
+                if (isPrime(i))
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+        public bool isPrime(int n)
+        {
+            if ((n % 2 == 0) && (n != 2))
+            {
+                return false;
+            }
+            int c = 3;
+            while ((c * c) <= n)
+            {
+                if (n % c == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    c += 2;
+                }
+            }
+            return true;
+        }
+    }
+}
+    
